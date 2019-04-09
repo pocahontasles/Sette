07d394e03f222d93dc9463bd2f6b7aa38f8e7947
Olesia <45497738+pocahontasles@users.noreply.github.com>
2019-04-09 11:21:35 +0300

    Add files via upload

--- build/js/contacts.js
+++ build/js/contacts.js
@@ -1,1 +1,1 @@
-jQuery(document).ready(function(){jQuery(".slider-contact-first").slick({slidesToShow:1,slidesToScroll:1,dots:!0,arrow:!1})}),jQuery(document).ready(function(){jQuery(".slider-contact-second").slick({slidesToShow:1,slidesToScroll:1,dots:!0,arrow:!1})});
+jQuery(document).ready(function(){jQuery(".slider-contact-first").slick({slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:5e3,dots:!0,arrow:!1})}),jQuery(document).ready(function(){jQuery(".slider-contact-second").slick({slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:5e3,dots:!0,arrow:!1})});
