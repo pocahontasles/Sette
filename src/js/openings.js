a0ae838edda256000928c2bbfe68fee5858354bc
Olesia <45497738+pocahontasles@users.noreply.github.com>
2019-04-10 14:35:21 +0300

    Delete openings.js

--- src/js/openings.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function () {
-    jQuery('.slider-openings').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        /*autoplay: true,
-        autoplaySpeed: 4000,*/
-        arrow: true
-    });
-});
